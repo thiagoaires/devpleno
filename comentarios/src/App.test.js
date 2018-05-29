ab8e9c52881dd80b05f4f77cedf428f4591efc31
Thiago Aires <thiago.aires@MAC-016.local>
2018-05-29 15:35:54 -0300

    devPleno - versao c/ bug no jest

--- comentarios/src/App.test.js
+++ comentarios/src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
-
 import {shallow, mount, render} from 'enzyme'
+import App from './App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
   const wrapper = shallow(<App />)
+  expect(wrapper.length).toBe(1)
   });
 })
