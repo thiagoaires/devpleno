8a97137595a5f4c042fb64e3bf2c184006b83a1b
Thiago Aires [note] <makarrao@gmail.com>
2018-05-28 21:44:30 -0300

    DevPleno - Modulo 4, config Enzyme

--- comentarios/src/App.test.js
+++ comentarios/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import {shallow, mount, render} from 'enzyme'
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  });
+})
