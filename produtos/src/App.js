5c5541ae97f6c95aa7220ebc51fa1cbbe0ff5043
Thiago Aires <makarrao@gmail.com>
2018-05-31 11:53:05 -0300

    devPleno - Produtos , estrutura inicial

--- produtos/src/App.js
+++ produtos/src/App.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <button className="btn btn-info">produto </button>
+      <div>
+        <nav className='navbar navbar-inverse'>
+          <div className='container'>
+            <div className='navbar-header'>
+              <a className='navbar-brand' href='/'>
+                Gerenciador de Produtos
+              </a>
+            </div>
+            
+          </div>
+        </nav>
       </div>
     );
   }
