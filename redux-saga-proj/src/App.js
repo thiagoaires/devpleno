a7434a253379e18ba6a1f8db0abc7434dd72c670
Thiago Aires <thiago.aires@MAC-016.local>
2018-07-23 15:59:06 -0300

    Redux trunk

--- redux-saga-proj/src/App.js
+++ redux-saga-proj/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux'  
 
 class App extends Component {
   render() {
